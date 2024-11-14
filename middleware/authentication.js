250649f22bd5eeddb75bb4c678aaf9125a3a4cc2
princess Ekere <pipicoker@gmail.com>
2024-11-14 15:34:08 +0100

    first commit

---
+++ middleware/authentication.js
@@ -0,0 +1,24 @@
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const {UnauthenticatedError} = require('../errors')
+
+const auth = async (req, res, next) => {
+    //check header
+    const authHeader = req.headers.authorization
+    if(!authHeader || !authHeader.startsWith('Bearer')){
+        throw new UnauthenticatedError('Authentication invalid')
+    }
+
+    const token = authHeader.split(' ')[1]
+
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+        // ATTACH THE USER TO THE JOB ROUTES
+        req.user = {userId:payload.userId, name:payload.name}
+    } catch (error) {
+        throw new UnauthenticatedError('aunthentication invalid')
+    }
+    next()
+}
+
+module.exports = auth
